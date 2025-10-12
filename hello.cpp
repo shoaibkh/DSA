a950377ef6b7485ec00921eef34dd7577f2e3104
Shoaib Ullah <shoaibullah@ideofuzion.com>
2025-10-13 00:01:49 +0500

    square pattern sequence

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void squarePattern( int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
-    }
-}
-
 int main() {
     squarePattern(5);
     // int a;
