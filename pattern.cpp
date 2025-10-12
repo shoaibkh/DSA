92585f55e33f728b5254a9c3a538007ee92be531
Shoaib Ullah <shoaibullah@ideofuzion.com>
2025-10-13 00:19:35 +0500

    continuousNumberPattern

--- pattern.cpp
+++ pattern.cpp
@@ -10,7 +10,19 @@ void squarePattern( int n){
     }
 }
 
+void continuousNumberPattern( int n){
+    int a=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+    }
+}
+
 int main() {
     squarePattern(5);
+    continuousNumberPattern(5);
     return 0;
 }
