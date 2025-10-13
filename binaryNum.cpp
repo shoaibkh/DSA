01aca0e1fae1df539dc1e4bd7cb3362a743e502c
Shoaib Ullah <shoaibullah@ideofuzion.com>
2025-10-13 23:47:46 +0500

    binaryToDec function

--- binaryNum.cpp
+++ binaryNum.cpp
@@ -12,7 +12,19 @@ int decToBinary(int decNum){
     return ans;
 }
 
+int binaryToDec(int binNum){
+    int ans = 0, pow = 1;
+    while(binNum>0){
+        int rem = binNum%10;
+        ans += (rem*pow);
+        binNum /= 10;
+        pow *= 2;
+    }
+    return ans;
+}
+
 int main(){
     cout<<decToBinary(3)<<endl;
+    cout<<binaryToDec(11)<<endl;
     return 0;
 }
