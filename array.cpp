e4d4756402f659c82dfe6f380e04a1cc2999df5e
Shoaib Ullah <shoaibkh587@gmail.com>
2025-10-14 23:50:22 +0500

    find index of min number and max number in array

--- array.cpp
+++ array.cpp
@@ -7,7 +7,7 @@ int findMin(){
     int marks[] = {10,20,30,40,50};
     int size = sizeof(marks)/sizeof(int);
     for(int i =0; i<size; i++){
-        smallest = max(marks[i], smallest);
+        smallest = min(marks[i], smallest);
     }
     return smallest;
 }
@@ -22,7 +22,37 @@ int findMax(){
     return maximum;
 }
 
+int findIndexOfMax(){
+    int largest = INT_MIN;
+    int marks[] = {10,20,60,40,50};
+    int size = sizeof(marks)/sizeof(int);
+    int index = 0;
+    for(int i =0; i<size; i++){
+        if(marks[i]>largest){
+            largest = marks[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+int findIndexOfMin(){
+    int smallest = INT_MAX;
+    int marks[] = {50,20,30,40,50};
+    int size = sizeof(marks)/sizeof(int);
+    int index = 0;
+    for(int i =0; i<size; i++){
+        if(smallest>marks[i]){
+            index = i;
+            smallest = marks[i];
+        }
+    }
+    return index;
+}
+
+
+
 int main(){
-    cout<<findMax()<<endl;
+    cout<<findIndexOfMin()<<endl;
     return 0;
 }
