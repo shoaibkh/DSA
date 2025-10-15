aa632945ae01e76719ac99212b78bbe86d5c8842
Shoaib Ullah <shoaibkh587@gmail.com>
2025-10-15 23:51:47 +0500

    reverse array through vector

--- vector.cpp
+++ vector.cpp
@@ -10,8 +10,27 @@ int singleNumber(vector<int> nums){
     }
     return ans;
 }
+
+vector<int> reverseArr(vector<int> nums){
+    int start = 0, end = nums.size()-1; 
+    // cout<<nums.at(end)<<endl;
+    while(end>start){
+        swap(nums.at(start), nums.at(end));
+        start++;
+        end--;
+    }
+    return nums;
+}
 int main(){
     vector<int> vec = {4,1,2,2,1};
-    cout<<singleNumber(vec)<<endl;
+    vector<int> reverseVec = reverseArr(vec);
+    for(int val: vec){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    for(int val: reverseVec){
+        cout<<val<<" ";
+    }
+    // cout<<singleNumber(vec)<<endl;
     return 0;
 }
