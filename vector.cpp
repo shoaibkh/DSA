d2db98c48e0f4b26558000be2a07c5b4b4effdb3
Shoaib Ullah <shoaibkh587@gmail.com>
2025-10-17 17:12:49 +0500

    maximum sub array sum function

--- vector.cpp
+++ vector.cpp
@@ -21,16 +21,42 @@ vector<int> reverseArr(vector<int> nums){
     }
     return nums;
 }
+
+int maximumSubArrSum(vector<int> nums){
+    int maxSum = INT_MIN;
+    for(int st=0; st<nums.size(); st++){
+        int currSum = 0;
+        for(int end = st; end<nums.size(); end++){
+            currSum += nums[end];
+            maxSum = max(currSum, maxSum);
+        }
+    }
+    return maxSum;
+}
+
+int maxSumByKadaneAlgo(vector<int> nums){
+    int maxSum = INT_MIN, currSum;
+    for(int i=0; i<nums.size(); i++){
+        currSum += nums[i];
+        maxSum = max(currSum, maxSum);
+        if(currSum<0){
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
+
 int main(){
-    vector<int> vec = {4,1,2,2,1};
-    vector<int> reverseVec = reverseArr(vec);
-    for(int val: vec){
-        cout<<val<<" ";
-    }
-    cout<<endl;
-    for(int val: reverseVec){
-        cout<<val<<" ";
-    }
+    vector<int> vec = {-4,-1,2,-2,1};
+    cout<<maxSumByKadaneAlgo(vec)<<endl;
+    // vector<int> reverseVec = reverseArr(vec);
+    // for(int val: vec){
+    //     cout<<val<<" ";
+    // }
+    // cout<<endl;
+    // for(int val: reverseVec){
+    //     cout<<val<<" ";
+    // }
     // cout<<singleNumber(vec)<<endl;
     return 0;
 }
